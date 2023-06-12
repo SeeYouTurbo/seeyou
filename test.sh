019ba596ca0841be06acc9c0e1ffc9245a6e5801
SeeYouTurbo <49407528+SeeYouTurbo@users.noreply.github.com>
2023-06-12 17:55:38 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,11 +6,7 @@ LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
-curl \
-  -H "Accept-Language: $LANGUAGE" \
-  -H "User-Agent: $UA" \
-  -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
-————————————————
-版权声明：本文为CSDN博主「黄河滴滴」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/qq_40748336/article/details/110749375
+curl --request GET \
+  --url 'http://127.0.0.1:18082/portal/odsDistributeStrategy/query?pageNum=1&pageSize=15&strategyChooseList=2' \
+  --header 'Token: NDNlNDZmNDU1MDRkNDc5YTk2MGYwOTM4ZmU4NWQwOWE=' \
+  --header 'content-type: multipart/form-data'
